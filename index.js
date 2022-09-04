fc7a66e0db43899afe2e3fb700874336325c44de
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-04 22:46:04 +0800

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { readdir } from 'fs/promises';
 import { createReadStream } from 'fs';
 import { createInterface } from 'readline';
 
-const recordPath = './video';
+const recordPath = '../video';
 (async () => {
     const files = await readdir(recordPath);
     for (const file of files) {
