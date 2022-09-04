cdf634ef2c5844ab028c1f3452d3de094efdb0bd
sixiwanzi-live <sixiwanzi.live@outlook.com>
2022-09-04 22:43:39 +0800

    dev

---
+++ index.js
@@ -0,0 +1,11 @@
+import { readdir } from 'fs/promises';
+import { createReadStream } from 'fs';
+import { createInterface } from 'readline';
+
+const recordPath = './video';
+(async () => {
+    const files = await readdir(recordPath);
+    for (const file of files) {
+        console.log(file);
+    }
+})();
