635a3d52871f78787ea04efd9ce5c8dce58a2aea
sixiwanzi-live <sixiwanzi.live@outlook.com>
2023-01-25 17:52:49 +0800

    dev

--- BililiveService.js
+++ BililiveService.js
@@ -76,8 +76,7 @@ export default class BililiveService {
         if (type === 'FileOpening') {
             ctx.logger.info('录制开始webhook'); 
             const datetime = body.EventData.FileOpenTime.substring(0, 19).replace('T', ' ');
-            // const roomId = body.EventData.RoomId;
-            let roomId = 25290861;
+            const roomId = body.EventData.RoomId;
             const name = body.EventData.Name;
             const title = body.EventData.Title.replaceAll('*', '_'); // 针对某些标题中含有*的情况，为了兼容windows系统文件，将*换成_
 
@@ -112,8 +111,7 @@ export default class BililiveService {
             }
         } else if (type === 'FileClosed') {
             ctx.logger.info('录制结束webhook');
-            // const roomId = body.EventData.RoomId;
-            let roomId = 25290861;
+            const roomId = body.EventData.RoomId;
             const name = body.EventData.Name;
             const title = body.EventData.Title;
             const duration = body.EventData.Duration;
